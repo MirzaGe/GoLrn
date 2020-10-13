3b451593fc898c041eac0d30de33e1d78d1342bf
sherry786 <shareyscorpi1@gmail.com>
2020-10-13 14:27:10 +0500

    commit

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
 	foo()
 	bar("james")
+	S1 := woo("MoneyPenny")
+	fmt.Println(S1)
 	fmt.Println("Hello" + "!" + "!")
 }
 func foo() {
@@ -13,4 +15,8 @@ func foo() {
 
 func bar(s string) {
 	fmt.Println("Hello,", s)
+}
+
+func woo(st string) string {
+	return fmt.Sprint("Hello from woo,", st)
 }
