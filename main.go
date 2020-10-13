323be268c6c516f69f962081442c0196441ff08e
sherry786 <shareyscorpi1@gmail.com>
2020-10-13 14:13:05 +0500

    functions

--- main.go
+++ main.go
@@ -3,7 +3,14 @@ package main
 import "fmt"
 
 func main() {
-
+	foo()
+	bar("james")
 	fmt.Println("Hello" + "!" + "!")
 }
-n
+func foo() {
+	fmt.Println("hello from foo")
+}
+
+func bar(s string) {
+	fmt.Println("Hello,", s)
+}
