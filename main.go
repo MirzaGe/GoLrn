3541829553fb171c116774c800c3c25e18e1ffea
sherry786 <shareyscorpi1@gmail.com>
2020-10-13 15:26:50 +0500

    jhon doe

--- main.go
+++ main.go
@@ -3,20 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	foo()
-	bar("james")
-	S1 := woo("MoneyPenny")
-	fmt.Println(S1)
-	fmt.Println("Hello" + "!" + "!")
-}
-func foo() {
-	fmt.Println("hello from foo")
+	fmt.Println(greet("Jane ", "Doe"))
 }
 
-func bar(s string) {
-	fmt.Println("Hello,", s)
-}
-
-func woo(st string) string {
-	return fmt.Sprint("Hello from woo,", st)
+func greet(fname string, lname string) (s string) {
+	s = fmt.Sprint(fname, lname)
+	return
 }
