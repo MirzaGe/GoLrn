066836f3c85e86f85d766ec9ee8f0ff3d66f64f9
sherry786 <shareyscorpi1@gmail.com>
2020-10-15 03:18:43 +0500

    call back

--- main.go
+++ main.go
@@ -3,10 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(bar()())
+	ii := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	s := sum(ii...)
+	fmt.Println("all number", s)
+	s2 := even(sum, ii...)
+	fmt.Println("even numbers", s2)
 }
-func bar() func() int {
-	return func() int {
-		return 451
+
+func sum(xi ...int) int {
+	fmt.Printf("%T\n", xi)
+	total := 0
+	for _, v := range xi {
+		total += v
 	}
+	return total
 }
+
+func even(f: func(xi ...int) int, vi...int) int {
+	var yi [] int
+	for _, v:= range vi {
+		if v % 2 == 0 {
+			yi = append(yi, v)
+		}
+	}
+	return f(yi...)
+
+}
+
