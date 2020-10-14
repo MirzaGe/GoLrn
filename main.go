2fa4b29a5511f9ef1ffeb99eed0e7409b4b5d8a9
sherry786 <shareyscorpi1@gmail.com>
2020-10-14 22:04:56 +0500

    defer

--- main.go
+++ main.go
@@ -4,40 +4,15 @@ import (
 	"fmt"
 )
 
-type person struct {
-	First string
-	Last  string
-	Age   int
-}
-
-type doubleZero struct {
-	person
-	First         string
-	LicenseToKill bool
-}
-
 func main() {
-	p1 := doubleZero{
-		person: person{
-			First: "James",
-			Last:  "Bond",
-			Age:   20,
-		},
-		First:         "Double Zero Seven",
-		LicenseToKill: true,
-	}
+	defer foo()
+	bar()
+}
 
-	p2 := doubleZero{
-		person: person{
-			First: "Mis",
-			Last:  "MoneyPenny",
-			Age:   19,
-		},
-		First:         "If looks could kill",
-		LicenseToKill: false,
-	}
+func foo() {
+	fmt.Println("foo")
+}
 
-	// fields and methods of the inner-type are promoted to the outer-type
-	fmt.Println(p1.First, p1.person.First)
-	fmt.Println(p2.First, p2.person.First)
+func bar() {
+	fmt.Println("bar")
 }
