7b82ffbafdacfaab6ca8edb4c3b03fb564244de6
sherry786 <shareyscorpi1@gmail.com>
2020-10-14 22:09:53 +0500

    methods

--- main.go
+++ main.go
@@ -4,15 +4,40 @@ import (
 	"fmt"
 )
 
+type person struct {
+	first string
+	last  string
+}
+
+type secretAgent struct {
+	person
+	ltk bool
+}
+
+// func (r receiver) identifier(parameters) (return(s)) { code }
+
+func (s secretAgent) speak() {
+	fmt.Println("I am", s.first, s.last)
+}
+
 func main() {
-	defer foo()
-	bar()
-}
+	sa1 := secretAgent{
+		person: person{
+			"James",
+			"Bond",
+		},
+		ltk: true,
+	}
 
-func foo() {
-	fmt.Println("foo")
-}
+	sa2 := secretAgent{
+		person: person{
+			"Miss",
+			"Moneypenny",
+		},
+		ltk: true,
+	}
 
-func bar() {
-	fmt.Println("bar")
+	fmt.Println(sa1)
+	sa1.speak()
+	sa2.speak()
 }
