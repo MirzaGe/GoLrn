59d2404cda1f534c58836751ac18ba69d082ae47
sherry786 <shareyscorpi1@gmail.com>
2020-10-15 19:16:13 +0500

    recursion2

--- main.go
+++ main.go
@@ -5,19 +5,4 @@ import "fmt"
 var x int
 
 func main() {
-	a := incrementor()
-	b := incrementor()
-	fmt.Println(a())
-	fmt.Println(a())
-	fmt.Println(a())
-	fmt.Println(b())
-
-}
-func incrementor() func() int {
-	var x int
-	return func() int {
-		x++
-		return x
-	}
-
-}
+fmt.Println(4)
