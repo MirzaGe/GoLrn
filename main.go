d49ae6a22493932fa71b23698657c126e4ea9ee2
sherry786 <shareyscorpi1@gmail.com>
2020-10-15 18:38:53 +0500

    scope

--- main.go
+++ main.go
@@ -1,29 +1,11 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+var x int
 
 func main() {
-	t := evenSum(sum, []int{1, 2, 3, 4, 5, 6, 7, 8, 9}...)
-	fmt.Println(t)
-}
-
-func sum(x ...int) int {
-	n := 0
-	for _, v := range x {
-		n += v
-	}
-	return n
-}
-
-func evenSum(f func(x ...int) int, y ...int) int {
-	var xi []int
-	for _, v := range y {
-		if v%2 == 0 {
-			xi = append(xi, v)
-		}
-	}
-	total := f(xi...)
-	return total
+	fmt.Println(x)
+	x++
+	fmt.Println(x)
 }
