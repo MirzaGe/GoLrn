ed19596ea888e68e204f53dd738f619fc4c68ded
sherry786 <shareyscorpi1@gmail.com>
2020-10-15 19:02:14 +0500

    closures

--- main.go
+++ main.go
@@ -5,7 +5,19 @@ import "fmt"
 var x int
 
 func main() {
-	fmt.Println(x)
-	x++
-	fmt.Println(x)
+	a := incrementor()
+	b := incrementor()
+	fmt.Println(a())
+	fmt.Println(a())
+	fmt.Println(a())
+	fmt.Println(b())
+
+}
+func incrementor() func() int {
+	var x int
+	return func() int {
+		x++
+		return x
+	}
+
 }
