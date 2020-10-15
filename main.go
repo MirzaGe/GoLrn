beae91cdf89289032568a879adc4816d23706b26
sherry786 <shareyscorpi1@gmail.com>
2020-10-16 02:52:31 +0500

    json

--- main.go
+++ main.go
@@ -3,7 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	a := 42
-	fmt.Println(a)
-	fmt.Println(&a)
+	x := 0
+	foo(x)
+	fmt.Println(x)
+}
+func foo(y int) {
+	fmt.Println(y)
+	y = 43
+	fmt.Println(y)
 }
