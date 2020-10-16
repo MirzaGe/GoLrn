26f4af2775b82847c7ff4719edf1c806615d8d41
sherry786 <shareyscorpi1@gmail.com>
2020-10-17 01:33:50 +0500

    sorting

--- main.go
+++ main.go
@@ -1,33 +1,20 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"sort"
 )
 
-type person struct {
-	First string `json:"First"`
-	Last  string `json:"Last"`
-	Age   int    `json:"Age"`
-}
-
 func main() {
-	s := `[{"First":"James","Last":"Bond","Age":32},{"First":"Miss","Last":"Moneypenny","Age":27}]`
-	bs := []byte(s)
-	fmt.Printf("%T\n", s)
-	fmt.Printf("%T\n", bs)
+	xi := []int{4, 7, 8, 55, 66, 77, 777, 7545}
+	xs := []string{"james", "Q", "M", "Moneypenny", "Dr. No"}
 
-	var people []person
+	fmt.Println(xi)
+	sort.Ints(xi)
+	fmt.Println(xi)
 
-	err := json.Unmarshal(bs, &people)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	fmt.Println("\nall of the data", people)
-
-	for i, v := range people {
-		fmt.Println("\nPERSON NUMBER", i)
-		fmt.Println(v.First, v.Last, v.Age)
-	}
+	fmt.Println("----------")
+	fmt.Println(xs)
+	sort.Strings(xs)
+	fmt.Println(xs)
 }
