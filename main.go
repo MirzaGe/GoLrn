e83703ffb43b9a0b58415f7b44ca95a0baf3f1b7
sherry786 <shareyscorpi1@gmail.com>
2020-10-17 01:35:52 +0500

    change

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	xi := []int{4, 7, 8, 55, 66, 77, 777, 7545}
+	xi := []int{444444, 7888, 98, 55, 66, 77, 777, 7545}
 	xs := []string{"james", "Q", "M", "Moneypenny", "Dr. No"}
 
 	fmt.Println(xi)
