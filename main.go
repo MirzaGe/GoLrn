da6493b46d035d4743c9a559740345ed41e7654d
sherry786 <shareyscorpi1@gmail.com>
2020-10-21 19:05:32 +0500

    buffer

--- main.go
+++ main.go
@@ -7,24 +7,7 @@ import (
 func main() {
 	c := make(chan int)
 
-	go send(c)
+	c <- 42
 
-	receive(c)
-
-	fmt.Println("about to exit")
-}
-
-// send channel
-func send(c chan<- int) {
-	for i := 0; i < 100; i++ {
-		c <- i
-	}
-	close(c)
-}
-
-// receive channel
-func receive(c <-chan int) {
-	for v := range c {
-		fmt.Println("the value received from the channel:", v)
-	}
+	fmt.Println(<-c)
 }
