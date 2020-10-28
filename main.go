8b0fe5ea912ed1924b86b81accfefd6e17281f2a
sherry786 <shareyscorpi1@gmail.com>
2020-10-28 23:26:57 +0500

    commit

--- main.go
+++ main.go
@@ -2,16 +2,13 @@ package main
 
 import "fmt"
 
+func zero(x *int) {
+	*x = 0
+}
 func main() {
-	a := 43
-	fmt.Println(a)
-	fmt.Println(&a)
-
-	var b *int = &a
-	fmt.Println(b)
-	fmt.Println(*b)
-	*b = 42
-	fmt.Println(a)
+	x := 5
+	zero(&x)
+	fmt.Println(x)
 }
 
 // constants,
