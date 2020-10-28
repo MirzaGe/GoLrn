a604474572f143ef8de7fb288842382167bb3ead
sherry786 <shareyscorpi1@gmail.com>
2020-10-28 23:12:40 +0500

    commit,

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
+
 import "fmt"
 
-const metersToYards float64 = 1.09361
 func main() {
-	var meters float64
-	fmt.Print("Enter meters swam: )
-	fmt.Scan(&meters)
-	yards := meters * metersToYards
-	fmt.Println(meters, " meters is: ", yards, "yards.")
+	a := 43
+	fmt.Println(a)
+	fmt.Println(&a)
+
+	var b *int = &a
+	fmt.Println(b)
+	fmt.Println(*b)
 }
 
 // constants,
