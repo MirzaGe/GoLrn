283edf4acd3aed8344564436ad06ed021da4194e
sherry786 <shareyscorpi1@gmail.com>
2020-10-29 21:48:33 +0500

    commit

--- main.go
+++ main.go
@@ -3,22 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	x := 11 % 3
-	fmt.Println(x)
-	if x == 1 {
-		fmt.Println("Odd")
-	} else {
-		fmt.Println("Even")
-	}
-
-	for i := 1; i < 10; i++ {
-		if i%2 == 1 {
-			fmt.Println("Odd")
-		} else {
-			fmt.Println("Even")
-		}
-	}
-
+	name := "Sherry"
+	fmt.Println("Hella", name)
 }
-
-// constants,
