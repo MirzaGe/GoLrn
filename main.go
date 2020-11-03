af0a5446dd628c36d8f8268328437ef47eb973cc
sherry786 <shareyscorpi1@gmail.com>
2020-11-04 01:23:23 +0500

    commit

--- main.go
+++ main.go
@@ -3,11 +3,4 @@ package main
 import "fmt"
 
 func main() {
-	student := make([]string, 32)
-	students := make([][]string, 32)
-	fmt.Println(student)
-	fmt.Println(students)
-	fmt.Println(student == nil)
-}
-
-//evenly
+	fmt.Println("Backlash: \\")
