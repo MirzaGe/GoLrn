0eb329edfab2f87421564fdd376406d9fbb3f251
sherry786 <shareyscorpi1@gmail.com>
2020-11-05 22:10:29 +0500

    commit

--- main.go
+++ main.go
@@ -2,12 +2,11 @@ package main
 
 import "fmt"
 
-func makeEvenGenerator() func() uint{
-	i := uint(0)
-	return func() (return uint){
-		ret = i
-		i += 2
-		return makeEvenGenerator()
+func main() {
+	defer func() {
+		str := recover()
+		fmt.Println(str)
+	}()
+	panic("PANIC")
 
-	}
 }
