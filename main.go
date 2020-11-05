b3ce7347fa389ea766958316704a22035f8ed3c4
sherry786 <shareyscorpi1@gmail.com>
2020-11-05 15:02:00 +0500

    commit

--- main.go
+++ main.go
@@ -3,13 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Enter a number: ")
-	var input float64
-	fmt.Scanf("%f", &input)
-	output := input * 0.3048
-	fmt.Println(output)
+	i := 1
+	for i <= 10 {
+		fmt.Println(i)
+		i = i + 1
+	}
 }
-
-/* Using the example program as a starting point,
-write a program that converts
-from Fahrenheit into Celsius (C = (F − 32) * 5/9). */
