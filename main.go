ae3bd347533ececee9bd3f7071b4c6a7193b2c53
sherry786 <shareyscorpi1@gmail.com>
2020-11-05 18:53:39 +0500

    commit

--- main.go
+++ main.go
@@ -3,9 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	for i := 1; i <= 100; i++ {
-		if i%3 == 0 {
-			fmt.Println(i)
-		}
-	}
+	slice1 := []int{1, 2, 3}
+	slice2 := make([]int, 2)
+	copy(slice2, slice1)
+	fmt.Println(slice1, slice2)
 }
