5f2bff0ca3b5a72b885536c3053a7594137c7611
sherry786 <shareyscorpi1@gmail.com>
2020-11-07 00:17:06 +0500

    commit

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
-
-type Circle struct {
-	x float64
-	y float64
-	r float64
-}
+import ( "fmt"
+	"time"
+	"math/rand"
+)
+func f(n int) { fori:=0;i<10;i++{
+	fmt.Println(n, ":", i)
+	amt := time.Duration(rand.Intn(250)) time.Sleep(time.Millisecond * amt)
+} }
+func main() { fori:=0;i<10;i++{
+	go f(i) }
+	var input string
+	fmt.Scanln(&input) }
