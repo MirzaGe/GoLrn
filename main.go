bffac62a0c415c378a163c043145be8dc9c71a97
sherry786 <shareyscorpi1@gmail.com>
2020-11-08 23:21:43 +0500

    commit

--- main.go
+++ main.go
@@ -1,27 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"net/http"
+	"github.com/chrislusf/gleam/flow"
 	"os"
 )
 
 func main() {
-	imageUrl := "https://golang.org/doc/gopher/doc.png"
-	response, err := http.Get(imageUrl)
-	if err != nil {
-		panic(err)
-	}
-	defer response.Body.Close()
-	file, err2 := os.Create("gopher.png")
-	if err2 != nil {
-		panic(err2)
-	}
-	_, err3 := io.Copy(file, response.Body)
-	if err3 != nil {
-		panic(err3)
-	}
-	file.Close()
-	fmt.Println("Image downloading is successful.")
+	flow.New().TextFile("/etc/paths").Partition(2).FlatMap(`
+function(line)
+return line:gmatch("%w+")
+end `).Map(`
+              function(word)
+                     return word, 1
+end `).ReduceBy(`
+              function(x, y)
+                     return x + y
+end
+`).Fprintf(os.Stdout, "%s,%dn").Run()
 }
